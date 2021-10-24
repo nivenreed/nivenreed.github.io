2a9fb28afc3ba64b6dbb504af1a9f727166bdbd2
nivenreed <nivenreed@hotmail.com>
2021-10-24 12:51:17 +0100

    Getting the menu to work

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 let toggleNavStatus = false;
 
 let toggleNav = function(){
-    let getDropDown = document.querySelector("overlay-content");
-    let getDropDownUL = document.querySelector("overlay-content ul");
-    let getDropDownLinks = document.querySelectoraAll("overlay-content a");
+    let getDropDown = document.querySelector("#my-nav");
+    let getDropDownUL = document.querySelector("#my-nav ul");
+    let getDropDownLinks = document.querySelectorAll("#my-nav a");
+    let menuVisible = document.querySelector("#menu-open");
+    let closeVisible = document.querySelector("#menu-close");
+    let menuLinkSummary = document.querySelectorAll(".menu-link-summary");
 
     if (toggleNavStatus === false){
-        getDropDownUL.style.visibility = "visable";
-        getDropDown.style.height = "95%"
+        getDropDownUL.style.visibility = "visible";
+        getDropDown.style.height = "95%";
+        getDropDown.style.visibility = "visible";
+        menuVisible.style.visibility = "hidden";
+        closeVisible.style.display = "block";
+        
 
         let arrayLength = getDropDownLinks.length;
-        for (let index = 0; index < array.length; index++) {
-            getDropDownLinks[index].style.opacity = "1"
+        for (let i = 0; i < arrayLength; i++) {
+            getDropDownLinks[i].style.visibility = "visible";
+        }
+        let arrayLength2 = menuLinkSummary.length;
+        for (let i = 0; i < arrayLength2; i++) {
+            menuLinkSummary[i].style.visibility = "visible";
         }
 
         toggleNavStatus = true;
@@ -22,10 +33,17 @@ let toggleNav = function(){
         getDropDown.style.height = "0"
 
         let arrayLength = getDropDownLinks.length;
-        for (let index = 0; index < array.length; index++) {
-            getDropDownLinks[index].style.opacity = "0"
+        for (let index = 0; index < arrayLength; index++) {
+            getDropDownLinks[index].style.visibility = "hidden"
+        }
+        let arrayLength2 = menuLinkSummary.length;
+        for (let i = 0; i < arrayLength2; i++) {
+            menuLinkSummary[i].style.visibility = "hidden";
         }
         getDropDownUL.style.visibility = "hidden";
+        getDropDown.style.visibility = "hidden";
+        menuVisible.style.visibility = "visible";
+        closeVisible.style.display = "none";
 
         toggleNavStatus = false;
     }
